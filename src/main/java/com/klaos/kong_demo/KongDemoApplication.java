27422cfba79a7c9c37aeac0e1263dc5c57134aa5
shilo0h <klaos.lleshi@fshnstudent.info>
2026-07-01 17:48:44 +0200

    Initial init

---
+++ src/main/java/com/klaos/kong_demo/KongDemoApplication.java
@@ -0,0 +1,13 @@
+package com.klaos.kong_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klaos/kong_demo/KongDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaos.kong_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
